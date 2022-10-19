67d9ad6e337ec68e9f90134be05c949e895e1e26
JagathPrasad <jagathprasad0@gmail.com>
2022-10-19 23:37:53 +0530

    rust file updated

--- anchor/todolist/lib.rs
+++ anchor/todolist/lib.rs
@@ -1,27 +1,29 @@
 use anchor_lang::prelude::*;
 
 pub mod constant;
+pub mod error;
 pub mod states;
-use create::{constant::*,states::*}
+use crate::{constant::*, error::*, states::*};
 // This is your program's public key and it will update
 // automatically when you build the project.
-declare_id!("11111111111111111111111111111111");
+declare_id!("6GWRcTdPQ17Uh6SkQs23i855nRzzHi3hrQTbZkiWhzZH");
 
 #[program]
-mod solana_todo {
+pub mod solana_todo {
     use super::*;
     pub fn initialize_user(ctx: Context<InitializeUser>) -> Result<()> {
         let user_profile = &mut ctx.accounts.user_profile;
         user_profile.authority = ctx.accounts.authority.key();
-        user_profile.last_todo=0;
-        user_profile.todo_count=0;
+        user_profile.last_todo = 0;
+        user_profile.todo_count = 0;
         Ok(())
     }
 
-    pub fn add_todo(ctx:Context<AddTodo>,_content:String)->Result<()>{
-        let todo_account= &mut ctx.accounts.todo_account;
+    pub fn add_todo(ctx: Context<AddTodo>, _content: String) -> Result<()> {
+        let todo_account = &mut ctx.accounts.todo_account;
         let user_profile = &mut ctx.accounts.user_profile;
-        todo_account.authority = ctx.accounts.authority.key;
+        todo_account.authority = ctx.accounts.authority.key();
+        todo_account.content = _content;
         todo_account.idx = user_profile.last_todo;
         todo_account.marked = false;
 
@@ -31,19 +33,19 @@ mod solana_todo {
         Ok(())
     }
 
-    pub fn mark_todo(ctx:Context<MarkTodo>,todo_idx:u8)->Result<()>{
+    pub fn mark_todo(ctx: Context<MarkTodo>, todo_idx: u8) -> Result<()> {
         //let user_profile = ctx.accounts.user_profile;
         let todo_account = &mut ctx.accounts.todo_account;
-        require!(!todo_account.marked, "CustomError");
-        todo_account.marked  = true;
-        Ok(());
+        require!(!todo_account.marked, TodoError::AlreadyMarked);
+        todo_account.marked = true;
+        Ok(())
     }
 
-      pub fn remove_todo(ctx:Context<MarkTodo>,todo_idx:u8)->Result<()>{
+    pub fn remove_todo(ctx: Context<RemoveTodo>, todo_idx: u8) -> Result<()> {
         //let user_profile = ctx.accounts.user_profile;
         let user_profile = &mut ctx.accounts.user_profile;
-           user_profile.todo_count = user_profile.todo_count.checked_sub(1).unwrap();
-       Ok(());
+        user_profile.todo_count = user_profile.todo_count.checked_sub(1).unwrap();
+        Ok(())
     }
 }
 
@@ -51,51 +53,49 @@ mod solana_todo {
 #[instruction()]
 pub struct InitializeUser<'info> {
     #[account(mut)]
-    pub authority:Signer<'info>,
+    pub authority: Signer<'info>,
     #[account(init,seeds=[USER_TAG,authority.key().as_ref()],bump, payer = authority, 
               space = 8 + std::mem:size_of::<UserProfile>())]
     pub user_profile: Box<Account<'info, UserProfile>>,
     pub system_program: Program<'info, System>,
 }
 
-
 #[derive(Accounts)]
 #[instruction()]
-pub struct AddTodo<'info>{
+pub struct AddTodo<'info> {
     #[account(mut,seeds=[USER_TAG,authority.key.as_ref()],bump,has_one=authority)]
-    pub user_profile:Box<Account<'info,UserProfile>>,
-    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref(),&[user_profile.last_todo].as_ref()],bump
+    pub user_profile: Box<Account<'info, UserProfile>>,
+    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref(),&[user_profile.last_todo.as_u8].as_ref()],bump
               ,payer=authority,space=8+ std::mem::size_of::<TodoAccout>())]
     pub todo_account: Box<Account<'info, TodoAccount>>,
 
-     #[account(mut)]
-    pub authority:Signer<'info>,
-    pub system_program:Program<'info,System>
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-
-#[derived(Accounts)]
+#[derive(Accounts)]
 #[instruction(todo_idx:u8)]
-pub struct MarkTodo<'info>{
+pub struct MarkTodo<'info> {
     #[account(mut,seeds=[USER_TAG,authority.key.as_ref()],bump,has_one=authority)]
-    pub user_profile:Box<Account<'info,UserProfile>>,
+    pub user_profile: Box<Account<'info, UserProfile>>,
 
-    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref()],&[todo_idx].as_ref(),bump,has_one = authority)]
-    pub todo_account :Box<Account<'info,TodoAccount>>,
-     #[account(mut)]
-    pub authority:Signer<'info>,
-    pub system_program:Program<'info,System>
+    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref(),&[todo_idx].as_ref()],bump,has_one = authority)]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
-#[derived(Accounts)]
+#[derive(Accounts)]
 #[instruction(todo_idx:u8)]
-pub struct RemoveTodo<'info>{
+pub struct RemoveTodo<'info> {
     #[account(mut,seeds=[USER_TAG,authority.key.as_ref()],bump,has_one=authority)]
-    pub user_profile:Box<Account<'info,UserProfile>>,
+    pub user_profile: Box<Account<'info, UserProfile>>,
 
-    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref()],&[todo_idx].as_ref(),bump,has_one = authority)]
-    pub todo_account :Box<Account<'info,TodoAccount>>,
-     #[account(mut)]
-    pub authority:Signer<'info>,
-    pub system_program:Program<'info,System>
+    #[account(mut,seeds=[TODO_TAG,authority.key.as_ref(),&[todo_idx].as_ref()],bump,has_one = authority)]
+    pub todo_account: Box<Account<'info, TodoAccount>>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
