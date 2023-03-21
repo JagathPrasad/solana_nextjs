53a4f227354e4ca97bbbcd287fb19008a7371736
JagathPrasad <jagathprasad0@gmail.com>
2023-03-21 13:15:16 +0530

    lottery lib.rs file updated

--- anchor/lottery/lib.rs
+++ anchor/lottery/lib.rs
@@ -4,6 +4,10 @@ use std::mem::size_of;
 // automatically when you build the project.
 declare_id!("GCm7dxw1MRUi38tx8xnDR1RPv45a5k2Fqv8cPCwrwaRo");
 
+pub const MASTER_SEED: &str = "master";
+pub const LOTTERY_SEED: &str = "lottery";
+pub const TICKET_SEED: &str = "ticket";
+
 #[program]
 mod lottery {
     use super::*;
@@ -13,9 +17,25 @@ mod lottery {
         Ok(())
     }
 
-    pub fn create_lottery(ctx: Context<CreateLottery>) -> Result<()> {
-        //let new_account =  &mut ctx.accounts.master_account;
-        //new_account.last_id = 0;
+    pub fn create_lottery(ctx: Context<CreateLottery>, ticket_price: u64) -> Result<()> {
+        let master = &mut ctx.accounts.master;
+        let lottery = &mut ctx.accounts.lottery;
+        master.last_id += 1;
+        lottery.id = master.last_id;
+        lottery.authority = ctx.accounts.authority.key();
+        lottery.ticket_price = ticket_price;
+        lottery.last_ticket_id = master.last_id;
+        //lottery.winner_id = master.last_id;
+        lottery.claimed = false;
+
+        Ok(())
+    }
+
+    pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
+        let lottery = &mut ctx.accounts.lottery;
+        let buyticket = &mut ctx.accounts.ticket;
+        let payer = &ctx.accounts.authority;
+
         Ok(())
     }
 }
@@ -24,7 +44,7 @@ mod lottery {
 pub struct InitMaster<'info> {
     #[account(init,
               payer = authority,
-              seeds=[b"master".as_ref(),authority.key().as_ref()],
+              seeds=[MASTER_SEED.as_bytes()],
               bump,
               space = size_of::<MasterAccount>() + 8
              )]
@@ -42,18 +62,37 @@ pub struct CreateLottery<'info> {
 
     #[account(init,
              payer=authority,
-             seeds=[b"lottery".as_ref(),authority.key().as_ref()],
+             seeds=[LOTTERY_SEED.as_bytes(),&(master.last_id + 1).to_le_bytes()],
              bump,
              space= size_of::<LotteryAccount>() + 8)]
-    pub lotter: Account<'info, LotteryAccount>,
+    pub lottery: Account<'info, LotteryAccount>,
     #[account(mut)]
     pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(lottery_id:u32)]
+pub struct BuyTicket<'info> {
+    #[account(mut)]
+    pub lottery: Account<'info, LotteryAccount>,
+
+    #[account(init,
+             payer=authority,
+             seeds=[TICKET_SEED.as_bytes(),lotter.key().as_ref(),&(lottery.last_ticket_id + 1).to_le_bytes()],
+             bump,
+             space=size_of::<TicketAccount>() + 8)]
+    pub ticket: Account<'info, TicketAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 pub struct MasterAccount {
-    pub last_id: u32,
+    pub last_id: u32, //4
 }
 
 #[account]
@@ -64,4 +103,11 @@ pub struct LotteryAccount {
     pub last_ticket_id: u32,
     pub winner_id: Option<u32>,
     pub claimed: bool,
+}
+
+#[account]
+pub struct TicketAccount {
+    pub id: u32,
+    pub authority: Pubkey,
+    pub lottery_id: u32,
 }
